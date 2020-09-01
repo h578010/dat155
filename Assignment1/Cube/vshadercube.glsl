df33c2f24b7419e16dd453c84fe0d86a1369f60a
Synnøve Sørensen <578010@stud.hvl.no>
2020-09-01 23:08:43 +0200

    Clean up

--- Assignment1/Cube/vshadercube.glsl
+++ Assignment1/Cube/vshadercube.glsl
@@ -4,10 +4,9 @@ in vec4 vPosition;
 in vec4 aColor;
 out vec4 vColor;
 
-uniform vec3 theta;
 uniform mat4 modelView;
 
 void main() {
-        vColor = aColor;
+    vColor = aColor;
     gl_Position = modelView * vPosition;
 }
